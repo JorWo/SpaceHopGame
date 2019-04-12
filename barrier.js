91a8d3396ddc316d6da5886f39c4121cf5671861
JorWo <42422209+JorWo@users.noreply.github.com>
2019-04-11 22:11:26 -1000

    Update barrier.js

--- barrier.js
+++ barrier.js
@@ -14,7 +14,8 @@ function barrier() {
   this.velocity = 0; //Velocity of barrier
   
   this.show = function() {
-    animation(meteorImg,this.x,this.y);
+    meteorSpr.position.x = this.x;
+    meteorSpr.position.y = this.y;
     }
     
   this.update = function() {
