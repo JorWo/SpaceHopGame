eba2b59d49a72287767289eaa7e9d08b8d402365
JorWo <42422209+JorWo@users.noreply.github.com>
2019-04-12 11:39:40 -1000

    Update barrier.js

--- barrier.js
+++ barrier.js
@@ -33,7 +33,7 @@ function barrier() {
     if (this.x < -75) { //barrier hits the left side
       this.x = w;
       this.y = Math.floor(Math.random()*h);
-      rand = random(0.2,3);
+      rand = random(0.3,3);
       this.randH = Math.floor(Math.random()*280) + 50;
       score++;
       
