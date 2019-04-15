c52469535f367f15a987737ec05bdfb2a8a69bf9
JorWo <42422209+JorWo@users.noreply.github.com>
2019-04-15 11:23:14 -1000

    Update jumper.js

--- jumper.js
+++ jumper.js
@@ -34,7 +34,7 @@ var jumperImg;
         }
         
         this.move = function() {
-          if (keyIsDown(68) && this.x < w) {
+          if (keyIsDown(68) && this.x < w-25) {
           this.x += 5;
           }
           if (keyIsDown(65) && this.x > 0) {
