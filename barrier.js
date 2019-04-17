6df75d1b3c20a035284393a289d0bfb29900bd6e
JorWo <42422209+JorWo@users.noreply.github.com>
2019-04-17 11:54:37 -1000

    Update barrier.js

--- barrier.js
+++ barrier.js
@@ -15,7 +15,7 @@ function barrier() {
   this.x = w;
   this.y = Math.floor(Math.random()*h);
   this.w = 50;
-  this.h = 300;
+  this.h = 500;
   this.gravity = 0.5; //The force of gravity
   this.velocity = 0; //Velocity of barrier
   
