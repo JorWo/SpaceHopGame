cdc636704870c822567a6910295ddfa312bfb237
JorWo <42422209+JorWo@users.noreply.github.com>
2019-04-19 12:14:26 -1000

    Update jumper.js

--- jumper.js
+++ jumper.js
@@ -58,6 +58,7 @@ var jumperImg;
       function keyPressed() {
         if (keyCode === 32) {
           jumper.up();
+          jumpSound.play()
         }
         if (keyCode === 82) {
           location.reload();
