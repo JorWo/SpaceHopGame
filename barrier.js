8ac66cf6463da0e6d097aff45eb64ce1e3c750b4
JorWo <42422209+JorWo@users.noreply.github.com>
2019-04-19 12:43:40 -1000

    Update barrier.js

--- barrier.js
+++ barrier.js
@@ -9,20 +9,19 @@ var rand;
 function setup() {
   meteorSpr.addAnimation("default", meteorImg); 
   meteorSpr = createSprite(this.x, this.y, this.randH, this.randH);
-  
 }
 
 function barrier() {
   this.x = w;
   this.y = Math.floor(Math.random()*h);
   this.w = 50;
-  this.h = 500;
   this.gravity = 0.5; //The force of gravity
   this.velocity = 0; //Velocity of barrier
  
   this.show = function() {
       meteorSpr.position.x = this.x;
       meteorSpr.position.y = this.y;
+      this.h = 500;
       meteorSpr.scale = rand;
     }
     
